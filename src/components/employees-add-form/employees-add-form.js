7973eb94b15090f35eeacaa2ab20cb91961773d9
Igor Zharov <stawr.900@gmail.com>
2022-07-20 16:11:15 +0300

    form_add_employees

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -7,13 +7,22 @@ class EmployeesAddForm extends Component {
         super(props);
         this.state = {
             name: '',
-            salary: 0
+            salary: ''
         }
     }
 
     onValueChange = (e) => {
         this.setState({
             [e.target.name] : e.target.value
+        })
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onAdd(this.state.name, this.state.salary);
+        this.setState({
+            name: '',
+            salary: ''
         })
     }
 
@@ -24,7 +33,8 @@ class EmployeesAddForm extends Component {
             <div className="app-add-form">
                 <h3>Добавьте нового сотрудника</h3>
                 <form
-                    className="add-form d-flex">
+                    className="add-form d-flex"
+                    onSubmit={this.onSubmit}>
                     <input type="text"
                         className="form-control new-post-label"
                         placeholder="Как его зовут?"
